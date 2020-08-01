6973e342e3000c3fb7669328831cfcc08699e668
Ludvig Böklin <ludvig.boklin@protonmail.com>
2020-08-01 20:49:25 +0200

    Fixed licenses. Many of which are unfree.

--- default.nix
+++ default.nix
@@ -1,45 +1,111 @@
 { stdenv, fontforge }:
 
-let mkDrv = name: version: stdenv.mkDerivation {
-      name = name;
+let mkDrv = font: stdenv.mkDerivation {
+      inherit (font) name;
       src = ./.;
       buildInputs = [ fontforge ];
       installPhase = ''
         mkdir -p $out/share/fonts/zbalermorna
-        ./build.sh ${name}
-        cp fonts/${name}.otf $out/share/fonts/zbalermorna
+        ./build.sh ${font.name}
+        cp fonts/${font.name}.otf $out/share/fonts/zbalermorna
       '';
 
       meta = with stdenv.lib; {
-        inherit version;
+        inherit (font) version license;
         description = "A font for the Zbalermorna writing system of the language Lojban";
         homepage = https://github.com/jackhumbert/zbalermorna;
-        license = with licenses; [ ofl ];
         platforms = platforms.all;
       };
     };
- in __mapAttrs (name: attrs: mkDrv name (__getAttr attrs name)) {
-      # I just got these with: grep -oE '^Version: .*$' src/*.sfd
-      balvi-regular        = { version = "1.001"; };
-      crisa-light          = { version = "2.015"; };
-      crisa-regular        = { version = "0.1"; };
-      dunda-regular        = { version = "1.10"; };
-      fira-code-zlm        = { version = "1.208"; };
-      lavi-zlm             = { version = "2.0"; };
-      lobster-zlm          = { version = "001.001"; };
-      nerfopi-regular      = { version = "001.001"; };
-      piper-karot          = { version = "1.1"; };
-      primihi-regular      = { version = "2008.02.06"; };
-      ritli-regular        = { version = "0.1"; };
-      tanbo-regular        = { version = "2.000"; };
-      tisna-bold           = { version = "0.1"; };
-      tnr-zlm              = { version = "001.000"; };
-      unicode-drakono      = { version = "001.000"; };
-      unicode-manri        = { version = "001.000"; };
-      unifont              = { version = "12.1.02"; };
-      vrude-italic-regular = { version = "1.200"; };
-      vrude-regular        = { version = "1.200"; };
-      zlm-manri            = { version = "001.000"; };
-      zlm-template         = { version = "0.1"; };
-      zlm-tnr              = { version = "001.000"; };
+ in with stdenv.lib.licenses;
+    __mapAttrs (name: attrs: mkDrv ({ name = name; } // attrs)) {
+      # I extracted versions with: grep -oE '^Version: .*$' src/*.sfd
+      balvi-regular        = {
+        version = "1.001";
+        license = ofl;
+      };
+      crisa-light          = {
+        version = "2.015";
+        license = ofl;
+      };
+      crisa-regular        = {
+        version = "0.1";
+        license = ofl;
+      };
+      dunda-regular        = {
+        version = "1.10";
+        license = unfree;
+      };
+      fira-code-zlm        = {
+        version = "1.208";
+        license = ofl;
+      };
+      lavi-zlm             = {
+        version = "2.0";
+        license = gpl3;
+      };
+      lobster-zlm          = {
+        version = "001.001";
+        license = unfree;
+      };
+      nerfopi-regular      = {
+        version = "001.001";
+        license = unfree;
+      };
+      piper-karot          = {
+        version = "1.1";
+        license = ofl;
+      };
+      primihi-regular      = {
+        version = "2008.02.06";
+        license = publicDomain;
+      };
+      ritli-regular        = {
+        version = "0.1";
+        license = gpl3;
+      };
+      tanbo-regular        = {
+        version = "2.000";
+        license = asl20;
+      };
+      tisna-bold           = {
+        version = "0.1";
+        license = asl20;
+      };
+      tnr-zlm              = {
+        version = "001.000";
+        license = unfree;
+      };
+      unicode-drakono      = {
+        version = "001.000";
+        license = unfree;
+      };
+      unicode-manri        = {
+        version = "001.000";
+        license = unfree;
+      };
+      unifont              = {
+        version = "12.1.02";
+        license = gpl2;
+      };
+      vrude-italic-regular = {
+        version = "1.200";
+        license = ofl;
+      };
+      vrude-regular        = {
+        version = "1.200";
+        license = ofl;
+      };
+      zlm-manri            = {
+        version = "001.000";
+        license = unfree;
+      };
+      zlm-template         = {
+        version = "0.1";
+        license = ofl;
+      };
+      zlm-tnr              = {
+        version = "001.000";
+        license = unfree;
+      };
     }
